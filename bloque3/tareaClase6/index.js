e7619a4c4211573de42dba2496f84277e371bd54
Monica1963 <amonica1963@gmail.com>
2020-11-26 23:24:43 -0300

    Ejercicio clase 6 con objetos

--- bloque3/tareaClase6/index.js
+++ bloque3/tareaClase6/index.js
@@ -1,7 +1,14 @@
+const usuario = {};
+
 const concatenar = () =>{
     const nombre = document.getElementById("nombre").value.toUpperCase();
     const apellido = document.getElementById("apellido").value.toUpperCase();
 
-    document.getElementById("nombreCompleto").value = nombre.concat(" " + apellido);
+    //document.getElementById("nombreCompleto").value = nombre.concat(" ", apellido);
+
+    //usando un objeto
+    Object.assign(usuario, {nombre, apellido});
+
+    document.getElementById("nombreCompleto").value = Object.values(usuario).join(" ");
 
 }
